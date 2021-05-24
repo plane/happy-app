8ee992f1cfe516906d434e54693f8946561a2406
Plane <crystal@panix.com>
2021-05-24 15:58:15 -0700

    Fix dependencies, bump version to 0.3

--- info.rkt
+++ info.rkt
@@ -2,12 +2,12 @@
 
 (define collection "happy-app")
 (define pkg-authors '("crystal@panix.com"))
-(define version "0.2")
+(define version "0.3")
 (define scribblings
  '(("happy-app.scrbl" () (library) "happy-app")))
 (define pkg-desc "Happy little #%app enhancement")
-(define deps '("base"
-               "ugly-app"))
+(define deps '(("base" "8.0")
+               "git://github.com/AlexKnauth/ugly-app?path=ugly-app"))
 (define build-deps '("rackunit-lib"
                      "racket-doc"
                      "scribble-lib"))
