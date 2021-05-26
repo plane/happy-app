721d7a0351d766d787c45b9d5499f2f603e143b6
Plane <crystal@panix.com>
2021-05-26 13:29:19 -0700

    Depend on pkgd.racket-lang.org version of ugly-app

--- info.rkt
+++ info.rkt
@@ -2,12 +2,12 @@
 
 (define collection "happy-app")
 (define pkg-authors '("crystal@panix.com"))
-(define version "0.5")
+(define version "0.6")
 (define scribblings
  '(("happy-app.scrbl" () (library) "happy-app")))
 (define pkg-desc "Happy little #%app enhancement")
 (define deps '("base"
-               "git://github.com/AlexKnauth/ugly-app?path=ugly-app"
+               "ugly-app-lib"
                "syntax-classes-lib"))
 (define build-deps '("rackunit-lib"
                      "racket-doc"
